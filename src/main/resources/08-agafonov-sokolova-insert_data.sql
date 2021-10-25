6edc1283d667b4ae9d6f65a7fc75a3006cb0edcd
uleeankin <uleeankin@gmail.com>
2021-10-25 09:55:48 +0300

    sql files

---
+++ src/main/resources/08-agafonov-sokolova-insert_data.sql
@@ -0,0 +1,11 @@
+insert into roles (id_role, name) values (0, 'Администратор')
+/
+insert into roles (id_role, name) values (1, 'Модератор')
+/
+insert into roles (id_role, name) values (2, 'Преподаватель')
+/
+insert into roles (id_role, name) values (3, 'Студент')
+/
+insert into users (id_user, login, password, first_name, last_name, middlename, role)
+values (0, 'admin', '7345admin2736', 'Соколова', 'Юлия', 'Анатольевна', 0)
+/
